214c7fec1494d48e93ca95f98691ec8eeaaadb2e
brandon.bielicki <brandon.bielicki@matrix.msu.edu>
2015-05-22 14:43:28 +0000

    Added ProjectsController for homepage

---
+++ app/Controller/ProjectsController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Projects Controller
+ * 
+ * @package    ARCS
+ * @link       http://github.com/calmsu/arcs
+ * @copyright  Copyright 2012, Michigan State University Board of Trustees
+ * @license    BSD License (http://www.opensource.org/licenses/bsd-license.php)
+ */
+class ProjectsController extends AppController {
+    public $name = 'Projects';
+	
+	public function beforeFilter() {
+        parent::beforeFilter();
+        $this->Auth->allow('display', 'search');
+		$this->set(array(
+		'toolbar' => false,
+        'footer' => false  
+		));
+    }
+	
+	/**
+     * Displays a view
+     *
+     * @param mixed What page to display
+     * @return void
+     */
+	public function display() {
+		$path = func_get_args();
+
+		$count = count($path);
+		if (!$count) {
+			$this->redirect('/');
+		}
+		$page = $subpage = $title_for_layout = null;
+
+		if (!empty($path[0])) {
+			$page = $path[0];
+		}
+		if (!empty($path[1])) {
+			$subpage = $path[1];
+		}
+		if (!empty($path[$count - 1])) {
+			$title_for_layout = Inflector::humanize($path[$count - 1]);
+		}
+        if ($title_for_layout == 'About') {
+            $this->set('toolbar', false);
+        }
+		$this->set(compact('page', 'subpage', 'title_for_layout'));
+		$this->render(implode('/', $path));
+	}
+
+}
