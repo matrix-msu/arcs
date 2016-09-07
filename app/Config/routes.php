fb75c2fb6d2956545ea34fc82aa29a37e80ff518
austin.rix <austin.rix@matrix.msu.edu>
2016-09-07 19:12:41 +0000

    Disabled auto routing organized and fixed routes

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -22,67 +22,85 @@
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-    # Home page
-    Router::connect('/',
-        array('controller' => 'projects', 'action' => 'display', 'index')
+
+////////////////////////////////////////////////////////////////////////
+# pages controller
+////////////////////////////////////////////////////////////////////////
+    # Pages routes
+    Router::connect('/pages/*',
+        array('controller' => 'pages', 'action' => 'display')
     );
-
-
-    Router::connect("/projects/single_project/*",
-        array('controller' => 'projects', 'action' => 'single_project')
-    );
-
-
-    Router::connect('/projects',
-        array('controller' => 'projects', 'action' => 'single_project', 'single_project')
-    );
-
-	# Resource page
+    # Resource page
     Router::connect('/resources',
         array('controller' => 'pages', 'action' => 'display', 'resources')
     );
-
     # Error pages
     Router::connect('/404',
         array('controller' => 'pages', 'action' => 'display', '404')
     );
+    # Cake Error page
     Router::connect('/500',
         array('controller' => 'pages', 'action' => 'display', '500')
     );
-
-
     # About
     Router::connect('/about',
         array('controller' => 'pages', 'action' => 'display', 'about')
     );
-
-    # Signup
-    Router::connect('/register/*',
-        array('controller' => 'users', 'action' => 'register')
-    );
-
-    # Invitation Signup
-    Router::connect('/invitation/register/*',
-        array('controller' => 'users', 'action' => 'registerByInvite')
-    );
-
-    # logout
-    Router::connect('/logout',
-        array('controller' => 'users', 'action' => 'logout')
-    );
-
     # Upload
     Router::connect('/upload',
         array('controller' => 'uploads', 'action' => 'batch')
     );
+////////////////////////////////////////////////////////////////////////
+# projects controller
+////////////////////////////////////////////////////////////////////////
+    # Home page
+    Router::connect('/',
+        array('controller' => 'projects', 'action' => 'display', 'index')
+    );
+    Router::connect("/projects/single_project/*",
+        array('controller' => 'projects', 'action' => 'single_project')
+    );
+/////////////////////////////////////////////////////////////////////////
+# resources controller
+////////////////////////////////////////////////////////////////////////
 
     # Resource, collection and user singular aliases
     Router::connect('/resource/*',
         array('controller' => 'resources', 'action' => 'viewer')
     );
-    Router::connect('/collection/*',
-        array('controller' => 'collections', 'action' => 'viewer')
+    # Non-RESTful API routes
+    Router::connect('/api/search',
+        array('controller' => 'search', 'action' => 'resources')
     );
+    Router::connect('/api/resources/keywords/*',
+        array('controller' => 'resources', 'action' => 'keywords')
+    );
+    Router::connect('/api/resources/comments/*',
+        array('controller' => 'resources', 'action' => 'keywords')
+    );
+    Router::connect('/api/resources/export',
+        array('controller' => 'resources', 'action' => 'export')
+    );
+    Router::connect('/resources/loadNewResource/*',
+        array('controller' => 'resources', 'action' => 'loadNewResource')
+    );
+    Router::connect('/resources/export',
+        array('controller' => 'resources', 'action' => 'export')
+    );
+
+////////////////////////////////////////////////////////////////////////
+# collections controller
+////////////////////////////////////////////////////////////////////////
+    # Collections page
+    Router::connect('/collections',
+        array('controller' => 'collections', 'action' => 'index')
+    );
+    Router::connect('/api/collections/memberships',
+        array('controller' => 'collections', 'action' => 'memberships')
+    );
+////////////////////////////////////////////////////////////////////////
+# users controller
+////////////////////////////////////////////////////////////////////////
     Router::connect('/user/*',
         array('controller' => 'users', 'action' => 'profile')
     );
@@ -92,41 +110,61 @@
     Router::connect('/users/confirm_user/*',
         array('controller' => 'users', 'action' => 'confirm_user')
     );
-
-
-# Search
-    # (we're using the greedy pattern so that we can match urls with slashes,
-    # e.g. 'search/filetype:application/pdf')
-
-
-
-    # We can access the JSON search through either /api/search or this:
-    # Router::connect('/resources/search/*',
-    #     array('controller' => 'search', 'action' => 'resources')
-    # );
-
-    # We can access the JSON search through either /api/search or this:
+    Router::connect('/users/confirm_user/*',
+        array('controller' => 'users', 'action' => 'confirm_user')
+    );
+    Router::connect('/users/special_login',
+        array('controller' => 'users', 'action' => 'special_login')
+    );
+    Router::connect('/api/users/invite',
+        array('controller' => 'users', 'action' => 'ajaxInvite')
+    );
+    # Signup
+    Router::connect('/register/*',
+        array('controller' => 'users', 'action' => 'register')
+    );
+    # Invitation Signup
+    Router::connect('/invitation/register/*',
+        array('controller' => 'users', 'action' => 'registerByInvite')
+    );
+    # logout
+    Router::connect('/logout',
+        array('controller' => 'users', 'action' => 'logout')
+    );
+////////////////////////////////////////////////////////////////////////
+# search controller
+////////////////////////////////////////////////////////////////////////
     Router::connect('/simple_search/*',
         array('controller' => 'search', 'action' => 'simple_search')
-        // array('controller' => 'search', 'action' => 'sm')
     );
-
-
     Router::connect('/api/simple_search/***',
         array('controller' => 'search', 'action' => 'simple_search')
-        // array('controller' => 'search', 'action' => 'sm')
     );
-
-
     Router::connect('/simple_search/user_verify',
         array('controller' => 'search', 'action' => 'verify')
     );
-
     Router::connect('/api/simple_search/user_verify',
         array('controller' => 'search', 'action' => 'verify')
     );
-
-
+    Router::connect('/sites',
+        array('controller' => 'search', 'action' => 'getProjects')
+    );
+    Router::connect('/search/**',
+        array('controller' => 'search', 'action' => 'search')
+    );
+	   Router::connect('/search/paginate',
+        array('controller' => 'search', 'action' => 'paginate')
+    );
+    # We can access the JSON search through either /api/search or this:
+    Router::connect('/resources/search',
+        array('controller' => 'search', 'action' => 'resources')
+    );
+    Router::connect('/resources/complete',
+        array('controller' => 'search', 'action' => 'complete')
+    );
+////////////////////////////////////////////////////////////////////////
+# advancedSearch controller
+////////////////////////////////////////////////////////////////////////
     Router::connect('/advanced_search',
         array('controller' => 'AdvancedSearch', 'action' => 'advance_search')
     );
@@ -135,52 +173,26 @@
         array('controller' => 'AdvancedSearch', 'action' => 'advanced_search')
     );
 
-
     Router::connect('/api/advanced_search/*',
         array('controller' => 'AdvancedSearch', 'action' => 'advanced_search')
     );
-
-
-    Router::connect('/sites',
-        array('controller' => 'search', 'action' => 'getProjects')
-    );
-
-    Router::connect('/search/**',
-        array('controller' => 'search', 'action' => 'search')
-    );
-	Router::connect('/search/paginate',
-        array('controller' => 'search', 'action' => 'paginate')
-    );
-
-
-    # We can access the JSON search through either /api/search or this:
-    Router::connect('/resources/search',
-        array('controller' => 'search', 'action' => 'resources')
-    );
-    Router::connect('/resources/complete',
-        array('controller' => 'search', 'action' => 'complete')
-    );
-
-	Router::connect('/resources/flags/add',
+	  Router::connect('/resources/flags/add',
         array('controller' => 'flags', 'action' => 'add')
     );
-
-
+////////////////////////////////////////////////////////////////////////
+# redirects
+////////////////////////////////////////////////////////////////////////
     # Search must have a trailing slash, for the client-side code's sanity.
     Router::redirect('/search', 'search/');
     Router::redirect('/simple_search', 'simple_search/');
     Router::redirect('/advanced_search', 'advanced_search/');
-
     # Configuration status
     Router::redirect('/admin',
         array('controller' => 'admin', 'action' => 'status')
     );
-
-    # Pages routes
-    Router::connect('/pages/*',
-        array('controller' => 'pages', 'action' => 'display')
-    );
-
+////////////////////////////////////////////////////////////////////////
+# help Controller
+////////////////////////////////////////////////////////////////////////
     # Docs routes
     Router::connect('/help',
         array('controller' => 'help', 'action' => 'display', 'index')
@@ -188,46 +200,30 @@
     Router::connect('/help/*',
         array('controller' => 'help', 'action' => 'display')
     );
-
-    # Non-RESTful API routes
-    Router::connect('/api/search',
-        array('controller' => 'search', 'action' => 'resources')
+////////////////////////////////////////////////////////////////////////
+# annotations Controller
+////////////////////////////////////////////////////////////////////////
+    Router::connect('/api/annotations/findall',
+          array('controller' => 'annotations', 'action' => 'findAll')
     );
-    // Router::connect('/api/metadata/*',
-        // array('controller' => 'resources', 'action' => 'metadata')
-    // );
-    Router::connect('/api/resources/keywords/*',
-        array('controller' => 'resources', 'action' => 'keywords')
-    );
-    Router::connect('/api/resources/comments/*',
-        array('controller' => 'resources', 'action' => 'keywords')
-    );
-
-    Router::connect('/api/resources/export',
-        array('controller' => 'resources', 'action' => 'export')
-    );
-
-    Router::connect('/api/users/invite',
-        array('controller' => 'users', 'action' => 'ajaxInvite')
-    );
-
-	Router::connect('/api/annotations/findall',
-        array('controller' => 'annotations', 'action' => 'findAll')
-    );
-
-    Router::connect('/api/collections/memberships',
-        array('controller' => 'collections', 'action' => 'memberships')
-    );
-
+////////////////////////////////////////////////////////////////////////
+# MetadataEdits Controller
+////////////////////////////////////////////////////////////////////////
     Router::connect('/metadataedits/add',
         array('controller' => 'MetadataEdits', 'action' => 'add')
     );
-Router::connect('/metadataedits/getAllKidsByScheme',
-    array('controller' => 'MetadataEdits', 'action' => 'getAllKidsByScheme')
-);
+    Router::connect('/metadataedits/getAllKidsByScheme',
+      array('controller' => 'MetadataEdits', 'action' => 'getAllKidsByScheme')
+    );
+////////////////////////////////////////////////////////////////////////
+# keywords Controller
+////////////////////////////////////////////////////////////////////////
     Router::connect('/keywords/add',
         array('controller' => 'keywords', 'action' => 'add')
     );
+////////////////////////////////////////////////////////////////////////
+##################
+////////////////////////////////////////////////////////////////////////
 
     $restful = array(
         'resources',
