5c1ae8a759c1a5a3074d691e427c836761ef82b2
matt.schleusener <matt.schleusener@matrix.msu.edu>
2017-05-22 18:23:47 +0000

    Model for the Mapping table

---
+++ app/Model/Mapping.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mapping extends AppModel {
+
+    public $name = 'Mapping';
+
+    public $whitelist = array(
+        'id_user', 'role', 'pid', 'status', 'activation'
+    );
+
+    /**
+     * Return a UUID suitable for account activation and reset tokens.
+     */
+    function getToken() {
+        App::uses('String', 'Utility');
+        return String::uuid();
+    }
+}
