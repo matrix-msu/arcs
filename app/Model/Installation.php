636a69b60d029b9811a4ba68ec0f1de8f984ee3e
noah.girard <noah.girard@matrix.msu.edu>
2018-03-09 21:08:40 +0000

    added installation model

---
+++ app/Model/Installation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Installation model
+ *
+ * @package    ARCS
+ * @link       http://github.com/calmsu/arcs
+ * @copyright  Copyright 2012, Michigan State University Board of Trustees
+ * @license    BSD License (http://www.opensource.org/licenses/bsd-license.php)
+ */
+class Installation extends AppModel {
+    public $name = 'Installation';
+}
