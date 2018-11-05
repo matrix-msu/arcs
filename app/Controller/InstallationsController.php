6febffada39b22f580c8eaf3a28385d0462a7f10
Josh Christ <josh.christ@matrix.msu.edu>
2018-11-05 17:42:21 +0000

    Update InstallationsController.php

--- app/Controller/InstallationsController.php
+++ app/Controller/InstallationsController.php
@@ -14,214 +14,233 @@
  App::import('Controller', 'Users');
 
  class InstallationsController extends AppController {
-	
-	public $name = 'Installations';
-	public $uses = array('User', 'Mapping');
 
-	public function beforeFilter() {
-		$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        $linkParts = explode("/", $actual_link);
+        public $name = 'Installations';
+        public $uses = array('User', 'Mapping');
+
+        public function beforeFilter() {
+            $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+            $linkParts = explode("/", $actual_link);
 
         if (CONFIGURED == 'true' && in_array('installation', $linkParts)) {
             $this->redirect('/');
         }
 
+        session_start();
+
         parent::beforeFilter();
         $this->Auth->allow(
-			'display', 'fieldConfig',
-			'createProject', 'arcsConfig', 'finalize'
-		);
-		if( $this->request->params['action'] != 'periodo' && $this->request->params['action'] != 'finalize' ){
-			echo "<script>var JS_IS_INSTALTION_PAGE = true;</script>";
-		}
+                        'display', 'fieldConfig',
+                        'createProject', 'arcsConfig', 'finalize'
+                );
+                if( $this->request->params['action'] != 'periodo' && $this->request->params['action'] != 'finalize' ){
+                        echo "<script>var JS_IS_INSTALTION_PAGE = true;</script>";
+                }
     }
 
-	public function register() {
-	          $this->set(array(
-	            'title_for_layout' => 'index'
-	        ));
-	        $this->render("register");
-	    }
+        public function register() {
+                  $this->set(array(
+                    'title_for_layout' => 'index'
+                ));
+                $this->render("register");
+            }
 
     /**
      * Displays the start installation page
      */
-	public function display() {
-	  	$this->set(array(
-	    	'title_for_layout' => 'Install ARCS'
-	    ));
-		$this->render("index");
-	}
+        public function display() {
+                $this->set(array(
+                'title_for_layout' => 'Install ARCS'
+            ));
+                $this->render("index");
+        }
 
-	/**
-	 * Displays the Kora Configuration page
-	 */
-	public function koraConfig() {
-		$this->set(array(
-			'title_for_layout' => 'Install ARCS'
-		));
-		$this->render("kora_config");
+        /**
+         * Displays the Kora Configuration page
+         */
+        public function koraConfig() {
+                $this->set(array(
+                        'title_for_layout' => 'Install ARCS'
+                ));
+                $this->render("kora_config");
 
-	}
+        }
 
-	/**
-	 * Displays the Field Configuration page
-	 */
-	public function fieldConfig() {
-		if($_POST){
-			$_SESSION['KoraConfig'] = $_POST;
-		}
-		
-		$this->set(array(
-			'title_for_layout' => 'Install ARCS'
-		));
-		$this->render("field_config");
-	}
+        /**
+         * Displays the Field Configuration page
+         */
+        public function fieldConfig() {
+                if($_POST){
+                        $_SESSION['KoraConfig'] = $_POST;
+                }
 
-	/**
-	 * Displays the Create Project page
-	 */
-	public function createProject() {
-		if($_POST){
-			$_SESSION['FieldConfig'] = $_POST;
-		}
+                $this->set(array(
+                        'title_for_layout' => 'Install ARCS'
+                ));
+                $this->render("field_config");
+        }
 
-		$this->set(array(
-			'title_for_layout' => 'Install ARCS'
-		));
-		$this->render("create_project");
-	}
+        /**
+         * Displays the Create Project page
+         */
+        public function createProject() {
+                if($_POST){
+                        $_SESSION['FieldConfig'] = $_POST;
+                }
 
-	/**
-	 * Displays the ARCS Configuration page
-	 */
-	public function arcsConfig() {
-		if($_POST){
-			$_SESSION['ProjectConfig'] = $_POST;
-		}
+                $this->set(array(
+                        'title_for_layout' => 'Install ARCS'
+                ));
+                $this->render("create_project");
+        }
 
-		$this->set(array(
-			'title_for_layout' => 'Install ARCS'
-		));
-		$this->render("arcs_config");
-	}
+        /**
+         * Displays the ARCS Configuration page
+         */
+        public function arcsConfig() {
+                if($_POST){
+                        $_SESSION['ProjectConfig'] = $_POST;
+                }
 
-	/*
-	* takes all user input and finalizes their kora installation by writing 
-	* straight to the kora DB
-	*/
-	public function finalize() {
-		if($_POST){
-			$_SESSION['ArcsConfig'] = $_POST;
-		}
-		//print_r(json_encode($_SESSION));die;
-		//write to koradb
+                $this->set(array(
+                        'title_for_layout' => 'Install ARCS'
+                ));
+                $this->render("arcs_config");
+        }
 
-		// HI JOSH THIS IS GOING TO BREAK
-		$host = $_SESSION['KoraConfig']['KoraDBHost'];
-		$username = $_SESSION['KoraConfig']['KoraDBUsername'];
-		$password = $_SESSION['KoraConfig']['KoraDBPassword'];
-		$dbName = $_SESSION['KoraConfig']['KoraDBName'];
+        /*
+        * takes all user input and finalizes their kora installation by writing
+        * straight to the kora DB
+        */
+        public function finalize() {
+                if($_POST){
+                        $_SESSION['ArcsConfig'] = $_POST;
+                }
 
-		$pName = trim(strtolower(str_replace(" ", "_", $_SESSION['KoraConfig']['KoraProjectName'])));
-		$pid = $GLOBALS['PID_ARRAY'][$pName];
-		
+                $host = KORA_HOST;
+                $username = KORA_USER;
+                $password = KORA_PASS;
+                $dbName = KORA_DB;
 
-		// Create connection
-		$conn = new mysqli($host, $username, $password, $dbName);
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-		//skip config, message, and auth which are automatically in a session variable
-		foreach($_SESSION as $key => $value){
-			if ($key == "Config" || $key == "Message" || $key == "Auth" || $key == "currentProjectName"){			
-				continue;
-			}
-			foreach($value as $key2 => $value2){
-				
-				$newKey = str_replace("_", " ", $key2);
-				$sql = $conn->prepare(
-					"SELECT * FROM kora3_fields 
-					WHERE NAME = ? AND pid = ?"
-				);
-				$sql->bind_param('ss', $newKey, $pid);
-				$result = $sql->execute();
+                $pName = trim(strtolower(str_replace(" ", "_", $_SESSION['ProjectConfig']['Persistent_Name'])));
+                $pid = $GLOBALS['PID_ARRAY']['arcs'];
 
-				if ($result->num_rows > 0) {
 
-					$insert = "";
-					$type = $result->fetch_assoc()['type'];
-					
-					if ($type == "List" || $type == "Multi-Select List") {
+                // Create connection
+                $conn = new mysqli($host, $username, $password, $dbName);
+                // Check connection
+                if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                }
+                //skip config, message, and auth which are automatically in a session variable
+                foreach($_SESSION as $key => $value){
+                        if ($key=="Config"||$key=="Message"||$key=="Auth"||$key=="currentProjectName"||$key=="ProjectConfig"||$key=="ArcsConfig"){     
+                                continue;
+                        }
+                        foreach($value as $key2 => $value2){
 
-						$insert .= "[!Options!]";
-						// $items = explode(",", $value2);
+                                $newKey = str_replace("_", " ", $key2);
+                                $sql = $conn->prepare(
+                                  
+                                        "SELECT * FROM kora3_fields
+                                        WHERE NAME = ? AND pid = ?"
+                                );
+//$sql = $conn->prepare("SELECT * FROM kora3_fields;");
+//var_dump('hi');die;
+                                $sql->bind_param('ss', $newKey, $pid);
+                                $sql->execute();
+                                $result = $sql->get_result();
+//var_dump($result);die;
+//var_dump($sql->fetch_assoc());die;
 
-						for ($i = 0; $i < sizeof($value2); $i++){
-							$insert .= trim($value2[$i]);
+                                if ($sql->num_rows > 0) {
 
-							if (($i+1) != sizeof($value2)) {
-								$insert .= "[!]";
-							}
-						}
+                                        $insert = "";
+                                        $type = $sql->fetch_assoc()['type'];
 
-						$insert .= "[!Options!]";  
-						
-						$sql = $conn->prepare(
-							"UPDATE kora3_fields 
-							SET options = ?
-							where name = ? and pid = ?"
-						); 
-						$sql->bind_param('sss', $insert, $newKey, $pid);
-						$sql->execute();
-					}
-				}
-			}
-		}
-		$conn->close();
+                                        if ($type == "List" || $type == "Multi-Select List") {
 
-		//create admin user
+                                                $insert .= "[!Options!]";
+                                                // $items = explode(",", $value2);
 
-		$usersC = new UsersController();
+                                                for ($i = 0; $i < sizeof($value2); $i++){
+                                                        $insert .= trim($value2[$i]);
 
-		$mappingProjects = array();
-		array_push($mappingProjects, array(
-			'project' => array('name' => $pName, 'pid' => $pid),
-			'role' => array('name' => 'Admin', 'value' => 'Admin')
-		));
+                                                        if (($i+1) != sizeof($value2)) {
+                                                                $insert .= "[!]";
+                                                        }
+                                                }
 
-		$addUserData = array(
-			'name' => $_SESSION['ArcsConfig']['ArcsAdminName'],
-            'username' => $_SESSION['ArcsConfig']['ArcsAdminUsername'],
-            'email' => $_SESSION['ArcsConfig']['ArcsAdminEmail'],
-            'password' => $_SESSION['ArcsConfig']['ArcsAdminPassword'],
-            'isAdmin' => 1,
-            'last_login' => null,
-            'status' => 'confirmed'
-		);
+                                                $insert .= "[!Options!]";
 
-		$response["status"] = $this->User->add($addUserData);
+                                                $sql = $conn->prepare(
+                                                        "UPDATE kora3_fields
+                                                        SET options = ?
+                                                        where name = ? and pid = ?"
+                                                );
+                                                $sql->bind_param('sss', $insert, $newKey, $pid);
+                                                $sql->execute();
+                                        }
+                                }
+                        }
+                }
+                $conn->close();
+
+                //create admin user
+
+                $usersC = new UsersController();
+
+                $mappingProjects = array();
+                array_push($mappingProjects, array(
+                        'project' => array('name' => $pName, 'pid' => $pid),
+                        'role' => array('name' => 'Admin', 'value' => 'Admin')
+                ));
+
+                $addUserData = array(
+                        'name' => $_SESSION['ArcsConfig']['ArcsAdminName'],
+                        'username' => $_SESSION['ArcsConfig']['ArcsAdminUsername'],
+                        'email' => $_SESSION['ArcsConfig']['ArcsAdminEmail'],
+                        'password' => $_SESSION['ArcsConfig']['ArcsAdminPassword'],
+                        'isAdmin' => 1,
+                        'last_login' => null,
+                        'status' => 'confirmed'
+                );
+
+                $response["status"] = $this->User->add($addUserData);
         if ($response["status"] == false) {
-			$response["message"] = $this->User->invalidFields();
-			return $this->json(400, ($response));
-		}
-		
+                        $response["message"] = $this->User->invalidFields();
+                        return $this->json(400, ($response));
+                }
+
         $usersC->editMappings($mappingProjects, array(), $response["status"]['User']['id']);
 
-		//write to bootstrap file so that configured = true
+                //write to bootstrap file so that configured = true
 
-		$path = APP . "Config/bootstrap.php";
-		$contents = file_get_contents($path);
-		$contents = str_replace(
-			"define('CONFIGURED', 'false');", 
-			"define('CONFIGURED', 'true');",
-			$contents
-		);
-		file_put_contents($path, $contents);
-		
-		$this->redirect('/');
-	}
+                $path = APP . "Config/bootstrap.php";
+                $contents = file_get_contents($path);
+                $contents = str_replace(
+                        "define('CONFIGURED', 'false');",
+                        "define('CONFIGURED', 'true');",
+                        $contents
+                );
+                $contents = str_replace(
+                        "'arcs' =>",
+                        "'".$pName."' =>",
+                        $contents
+                );
+                $contents = str_replace(
+                        'define("BASE_BOTH", "");',
+                        'define("BASE_BOTH", "'.$_SESSION['ArcsConfig']['ArcsBaseURL'].'");',
+                        $contents
+                );
+                file_put_contents($path, $contents);
+
+//jecho $content;
+//die;
+
+                $this->redirect('/');
+        }
 
     /**
      * Returns Original Label from Permalink URL
